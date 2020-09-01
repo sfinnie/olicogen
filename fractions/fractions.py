58f84f0d46e1f6f8fcddaf5ce98640ee7cfc68e8
Scott Finnie <scott.finnie@hymans.co.uk>
2020-09-01 17:59:43 +0100

    initial checkin prior to parameterising 100ths template

---
+++ fractions/fractions.py
@@ -0,0 +1,90 @@
+# Fractions
+#
+# Code to generate example fraction questions that can be uploaded to the Olico website.
+#
+#
+import random
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+# from pylatex import Document, Section, Subsection, TikZ, Math, Axis, Plot
+# from pylatex.utils import italic
+
+
+def generate_questions(num_questions=10, min=1, max=100):
+    """ generate the specified number of questions, using min and max as the upper and lower bounds
+        For example:
+
+            generate_questions(10, 1, 100) will generate 10 questions where the answer is between 1 and 100.
+
+            So we might get questions for: 9/100, 15/100, 21/100... 97/100
+
+            Each question comprises:
+            1. The question text
+            2. A diagram showing the number line
+            3. The correct answer
+    """
+    values=random.sample(range(min, max), num_questions)
+    questions = []
+    for v in values:
+        q = generate_question(v)
+        questions.append(q)
+    return questions
+
+
+def generate_question(value):
+    """returns a question where the correct answer is `value`"""
+    diagram = generate_diagram(value)
+    return diagram
+
+
+def generate_diagram(value):
+
+    env = Environment(
+        loader=FileSystemLoader("templates"),
+        autoescape=select_autoescape(['xml'])
+    )
+
+    template = env.get_template("hundreds.tex")
+    doc = template.render()
+    with open(f"example-{value}.tex", "w") as fh:
+        fh.write(doc)
+
+# def generate_diagram(value):
+#     """ generates a number line showing `value` 100ths coloured in
+#         Uses the LaTeX Tikz package
+#     """
+#     geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
+#     doc = Document(geometry_options=geometry_options)
+#
+#     with doc.create(Section('The simple stuff')):
+#         doc.append('Some regular text and some')
+#         doc.append(italic('italic text. '))
+#         doc.append('\nAlso some crazy characters: $&#{}')
+#         with doc.create(Subsection('Math that is incorrect')):
+#             doc.append(Math(data=['2*3', '=', 9]))
+#
+#         with doc.create(Subsection('Beautiful graphs')):
+#             with doc.create(TikZ()):
+#                 plot_options = 'height=4cm, width=6cm, grid=major'
+#                 with doc.create(Axis(options=plot_options)) as plot:
+#                     plot.append(Plot(name='model', func='-x^5 - 242'))
+#
+#                     coordinates = [
+#                         (-4.77778, 2027.60977),
+#                         (-3.55556, 347.84069),
+#                         (-2.33333, 22.58953),
+#                         (-1.11111, -493.50066),
+#                         (0.11111, 46.66082),
+#                         (1.33333, -205.56286),
+#                         (2.55556, -341.40638),
+#                         (3.77778, -1169.24780),
+#                         (5.00000, -3269.56775),
+#                     ]
+#
+#                     plot.append(Plot(name='estimate', coordinates=coordinates))
+#     doc.generate_pdf(f"question-{value}.pdf")
+#     return doc
+
+
+if __name__ == "__main__":
+    generate_questions(10, 1, 100)
